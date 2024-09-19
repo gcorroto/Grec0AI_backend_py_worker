dcb45817097d210595e19c6516f7a575e467ecd7
gcorroto@profile.es <gcorroto@profile.es>
2024-09-19 18:56:11 +0200

    Refactor RedisService to use IP address instead of hostname for Redis configuration

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -2,7 +2,7 @@ import redis
 
 class RedisService:
     def __init__(self):
-        self.r = redis.StrictRedis(host='redis', port=6379, db=0)  # Configuración de Redis
+        self.r = redis.StrictRedis(host='172.25.0.3', port=6379, db=0)  # Configuración de Redis
 
     def get_next_script(self):
         """Obtiene el siguiente script de la cola en Redis."""
